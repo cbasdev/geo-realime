4a23d777fd54c879d9e8e16444c09f483ec9a218
cbasdev <>
2022-04-06 09:52:41 -0500

    feat - vite and tools upgrading

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>&nbsp;
+    <router-link to="/about">Acerca de</router-link>
+  </nav>
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
